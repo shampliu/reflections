3b3feebb6d94bf60c0ea09278a1ee5173ba3697a
Chang Liu <shampliu@gmail.com>
2024-05-27 23:15:10 -0400

    Ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // to prevent mount, remount, mount
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
